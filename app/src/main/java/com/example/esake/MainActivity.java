f76c6d290d109856623f2a742cd7af43506eacb1
Ntinos <konstantinides.kon@gmail.com>
2022-05-16 12:02:03 +0300

    Test 2

--- app/src/main/java/com/example/esake/MainActivity.java
+++ app/src/main/java/com/example/esake/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        City newCity = new City("test", "test");
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
