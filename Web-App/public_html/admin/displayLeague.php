e11c6bc0f8716ed9faf0d628f2e6ba7862c7efe8
iosif199 <iosifsaad@gmail.com>
2022-05-24 17:27:57 +0300

    Fixed a typo in 'login'

--- Web-App/public_html/admin/displayLeague.php
+++ Web-App/public_html/admin/displayLeague.php
@@ -46,39 +46,7 @@ $err_msg = 'Ενα μήνυμα σφάλματος';
 	<body class="d-flex flex-column h-100">
 	
 		<header>
-			<!-- Fixed navbar -->
-			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-				<div class="container-fluid">
-				<a class="navbar-brand" href="#">ΕΣΑΚΕ Managment App</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarCollapse">
-					<ul class="navbar-nav me-auto mb-2 mb-md-0">
-					
-					<li class="nav-item">
-						<a class="nav-link" aria-current="page" href="./">Αρχική</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="./createLeague.php">Δημιουργία Πρωταθλήματος</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="./createTeam.php">Δημιουργία Ομάδας</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="./createPlayer.php">Δημιουργία Παίκτη</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link active" href="./drawLeague.php">Κλήρωση Πρωταθλήματος</a>
-					</li>
-					
-					</ul>
-					<form class="d-flex" action="../logout.php">
-						<button class="btn btn-outline-danger" type="submit">Αποσύνδεση</button>
-					</form>
-				</div>
-				</div>
-			</nav>
+			<?php require_once MAIN_NAVIGATION ?>
 		</header>
 
 		<!-- Begin page content -->
@@ -164,12 +132,7 @@ $err_msg = 'Ενα μήνυμα σφάλματος';
 		</main>
 
 		<!-- Footer -->
-		<footer class="footer mt-auto py-3 bg-dark">
-			<div class="container">
-				<span class="text-muted">Εργασία Εξαμήνου, Ανάπτυξη Εφαρμογών για Κινητές Συσκευές - 6ο Εξάμηνο.</span><br>
-				<span class="text-muted">&copy; Copyright Ομάδα 1, 2021-2022 .</span>
-			</div>
-		</footer>
+		<?php require_once MAIN_FOOTER ?>
 	</body>
 	
 </html>
--- Web-App/public_html/admin/index.php
+++ Web-App/public_html/admin/index.php
@@ -1,10 +1,16 @@
 <?php
 
 session_start();
+require_once '../../resources/config.php';
+
+$currPage = 'start';
+
+
 
 // If the user is not logged in, he gets redirected at the loggin page.
 if(!isset($_SESSION["logged_in"]) || !$_SESSION["logged_in"] === true) {
-	header('Location: ../loggin/?r=true');
+	echo 'Location: ' . DIR_LOGIN;
+	header('Location: ' . DIR_LOGIN . 'index.php');
 	die();
 }
 
@@ -32,39 +38,7 @@ if(!isset($_SESSION["logged_in"]) || !$_SESSION["logged_in"] === true) {
 	<body class="d-flex flex-column h-100">
 	
 		<header>
-			<!-- Fixed navbar -->
-			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-				<div class="container-fluid">
-				<a class="navbar-brand" href="#">ΕΣΑΚΕ Managment App</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarCollapse">
-					<ul class="navbar-nav me-auto mb-2 mb-md-0">
-					
-					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="#">Αρχική</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="./createLeague.php">Δημιουργία Πρωταθλήματος</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="./createTeam.php">Δημιουργία Ομάδας</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="./createPlayer.php">Δημιουργία Παίκτη</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="./drawLeague.php">Κλήρωση Πρωταθλήματος</a>
-					</li>
-					
-					</ul>
-					<form class="d-flex" action="../logout.php">
-						<button class="btn btn-outline-danger" type="submit">Αποσύνδεση</button>
-					</form>
-				</div>
-				</div>
-			</nav>
+			<?php require_once MAIN_NAVIGATION ?>
 		</header>
 
 		<!-- Begin page content -->
@@ -93,12 +67,7 @@ if(!isset($_SESSION["logged_in"]) || !$_SESSION["logged_in"] === true) {
 		</main>
 
 		<!-- Footer -->
-		<footer class="footer mt-auto py-3 bg-dark">
-			<div class="container">
-				<span class="text-muted">Εργασία Εξαμήνου, Ανάπτυξη Εφαρμογών για Κινητές Συσκευές - 6ο Εξάμηνο.</span><br>
-				<span class="text-muted">&copy; Copyright Ομάδα 1, 2021-2022 .</span>
-			</div>
-		</footer>
+		<?php require_once MAIN_FOOTER ?>
 	</body>
 	
 </html>
