eb4062a226478c098730610db193fbec82d5a715
Ntinos <konstantinides.kon@gmail.com>
2022-05-29 15:22:52 +0300

    Added live team manage fragment

--- app/src/main/java/com/example/esake/FragmentTeamManagementStatsManager.java
+++ app/src/main/java/com/example/esake/FragmentTeamManagementStatsManager.java
@@ -59,6 +59,6 @@ public class FragmentTeamManagementStatsManager extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team_management_stats_manager, container, false);
+        return inflater.inflate(R.layout.fragment_team_management_startingteam_stats_manager, container, false);
     }
 }
