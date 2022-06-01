971af1c8c6ff9ab00081653dc88a6cdd2e26af07
Ntinos <konstantinides.kon@gmail.com>
2022-06-01 11:41:28 +0300

    Minor Changes

--- app/src/main/java/com/example/esake/FragmentUserHome.java
+++ app/src/main/java/com/example/esake/FragmentUserHome.java
@@ -9,46 +9,16 @@ import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentUserHome#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentUserHome extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    /**
-     *
-     * Testing Database connection with the App
-     *
-     *
-    DatabaseAdapter databaseAdapter;
-    RecyclerView rvGameweek;
-    GamesAdapter gamesAdapter;
-    RecyclerView.LayoutManager layoutManager;
-    List<Game> gamesList = new ArrayList<>(); */
+    public FragmentUserHome() {}
 
-    public FragmentUserHome() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentUserHome.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentUserHome newInstance(String param1, String param2) {
         FragmentUserHome fragment = new FragmentUserHome();
         Bundle args = new Bundle();
@@ -71,18 +41,17 @@ public class FragmentUserHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_home_user, container, false);
+        // Return inflater.inflate(R.layout.fragment_home_user, container, false);
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_home_user, null);
-        //get the spinner from the xml.
+        // Get the spinner from the xml.
         Spinner dropdown = (Spinner) root.findViewById(R.id.gameweek_selection);
-        //create a list of items for the spinner.
+        // Create a list of items for the spinner.
         String[] items = new String[]{"Gameweek 1", "Gameweek 2", "Gameweek 3", "etc..."};
-        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-        //There are multiple variations of this, but this is the basic variant.
+        // Create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        // There are multiple variations of this, but this is the basic variant.
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(root.getContext(), android.R.layout.simple_spinner_dropdown_item, items);
-        //set the spinners adapter to the previously created one.
+        // Set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
         return root;
     }
-
 }
