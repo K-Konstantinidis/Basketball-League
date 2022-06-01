12eea9e3963047ae22523f8956ef59cbc5f24a15
Giannis <johhni97@gmail.com>
2022-06-01 11:59:12 +0300

    test

---
+++ app/src/main/java/com/example/esake/StatsManagerHome/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.esake.StatsManagerHome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.esake.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home_stats_manager);
+    }
+
+}
