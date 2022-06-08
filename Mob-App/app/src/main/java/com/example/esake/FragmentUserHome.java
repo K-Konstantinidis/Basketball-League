d472f3ca88f3f3eb7f185ccbc784b319be315ca4
Stavros Vargemezis <stavrosvarg@gmail.com>
2022-06-08 03:59:58 +0300

    (08/06) Connected Database with App to display proper Gameweek Info on User Home spinner

--- Mob-App/app/src/main/java/com/example/esake/FragmentUserHome.java
+++ Mob-App/app/src/main/java/com/example/esake/FragmentUserHome.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
@@ -18,7 +16,10 @@ public class FragmentUserHome extends Fragment {
 //    private String mParam1;
 //    private String mParam2;
 
-    public FragmentUserHome() {}
+	Spinner gameweekSpinner;
+	static String urlAddress = "http://192.168.1.7/android/populateRounds.php";
+
+	public FragmentUserHome() {}
 
 //    public static FragmentUserHome newInstance(String param1, String param2) {
 //        FragmentUserHome fragment = new FragmentUserHome();
@@ -38,29 +39,35 @@ public class FragmentUserHome extends Fragment {
 //        }
 //    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-    	//Get the view
-    	View v = inflater.inflate(R.layout.fragment_home_user, null);
-        // Inflate the layout for this fragment
-        // Return inflater.inflate(R.layout.fragment_home_user, container, false);
-        //@SuppressLint("InflateParams") ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_home_user, null);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		//Get the view
+		View view = inflater.inflate(R.layout.fragment_home_user, null);
+
+		gameweekSpinner = (Spinner) view.findViewById(R.id.gameweek_selection_spinner);
+		Downloader downloader = new Downloader(view.getContext(), urlAddress, gameweekSpinner);
+		downloader.execute();
+
+
+		// Inflate the layout for this fragment
+		// Return inflater.inflate(R.layout.fragment_home_user, container, false);
+		//@SuppressLint("InflateParams") ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_home_user, null);
 
 		// Get the spinner
-        Spinner dropdown = (Spinner) v.findViewById(R.id.gameweek_selection);
-
-        // Get the button
-        Button game = v.findViewById(R.id.gameweek_preview_gameButton);
-
-        // Create a list of items for the spinner.
-        String[] items = new String[]{"Gameweek 1", "Gameweek 2", "Gameweek 3", "etc..."};
-
-        // Create an adapter to describe how the items are displayed, adapters are used in several places in android.
-        // There are multiple variations of this, but this is the basic variant.
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_spinner_dropdown_item, items);
-        // Set the spinners adapter to the previously created one.
-        dropdown.setAdapter(adapter);
-        return v;
-    }
+//		Spinner dropdown = (Spinner) v.findViewById(R.id.gameweek_selection_spinner);
+//
+//		// Get the button
+//		Button game = v.findViewById(R.id.gameweek_preview_gameButton);
+//
+//		// Create a list of items for the spinner.
+//		String[] items = new String[]{"Gameweek 1", "Gameweek 2", "Gameweek 3", "etc..."};
+//
+//		// Create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//		// There are multiple variations of this, but this is the basic variant.
+//		ArrayAdapter<String> adapter = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_spinner_dropdown_item, items);
+//		// Set the spinners adapter to the previously created one.
+//		dropdown.setAdapter(adapter);
+		return view;
+	}
 }
--- Mob-App/app/src/main/java/com/example/esake/GamesAdapter.java
+++ Mob-App/app/src/main/java/com/example/esake/GamesAdapter.java
@@ -11,62 +11,62 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class GamesAdapter extends RecyclerView.Adapter<GamesAdapter.ViewHolder>{
+public class GamesAdapter extends RecyclerView.Adapter<GamesAdapter.GameViewHolder> {
 
-    Context context;
-    List<Game> gamesList;
-    RecyclerView rvGameweek;
-    final View.OnClickListener onClickListener = new MyOnClickListener();
-    public static class ViewHolder extends RecyclerView.ViewHolder{
-//        ImageView team1Logo;
-//        ImageView team2Logo;
-        TextView score1;
-        TextView score2;
+	private Context mCtx;
+	private List<Game> gamesList;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            score1 = itemView.findViewById(R.id.gameweek_preview_team1_score);
-            score2 = itemView.findViewById(R.id.gameweek_preview_team2_score);
-//            team1Logo = itemView.findViewById(R.id.fixture_team1_logo);
-//            team2Logo = itemView.findViewById(R.id.fixture_team2_logo);
-        }
-    }
+	public GamesAdapter(Context mCtx, List<Game> gamesList){
+		this.mCtx = mCtx;
+		this.gamesList = gamesList;
+	}
 
-    public GamesAdapter(Context context, List<Game> gamesList, RecyclerView rvGameweek){
-        this.context = context;
-        this.gamesList = gamesList;
-        this.rvGameweek = rvGameweek;
-    }
+	@NonNull
+	@Override
+	public GamesAdapter.GameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		LayoutInflater inflater = LayoutInflater.from(mCtx);
+		View view = inflater.inflate(R.layout.single_fixture, null);
+		return new GameViewHolder(view);
+	}
 
-    @NonNull
-    @Override
-    public GamesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.single_fixture, viewGroup, false);
-        view.setOnClickListener(onClickListener);
+	@Override
+	public void onBindViewHolder(@NonNull GamesAdapter.GameViewHolder holder, int position) {
+		Game game = gamesList.get(position);
 
-        return new ViewHolder(view);
-    }
+		//loading the image
+//        Glide.with(mCtx)
+//                .load(product.getImage())
+//                .into(holder.imageView);
+//
+//        holder.textViewTitle.setText(product.getTitle());
+//        holder.textViewShortDesc.setText(product.getShortdesc());
+//        holder.textViewRating.setText(String.valueOf(product.getRating()));
+//        holder.textViewPrice.setText(String.valueOf(product.getPrice()));
+//		holder.roundId.setText(String.valueOf(game.getRoundId()));
+//		holder.homeTeamId.setText(String.valueOf(game.getHomeTeamId()));
+//		holder.awayTeamId.setText(String.valueOf(game.getAwayTeamId()));
+	}
 
-    @Override
-    public void onBindViewHolder(@NonNull GamesAdapter.ViewHolder viewHolder, int i) {
-        Game game = gamesList.get(i);
-        viewHolder.score1.setText(""+game.getScore1());
-        viewHolder.score2.setText(""+game.getScore2());
-//        viewHolder.team1Logo.setImageDrawable(R.drawable.);
-    }
+	@Override
+	public int getItemCount() {
+		return gamesList.size();
+	}
 
-    @Override
-    public int getItemCount() {
-        return gamesList.size();
-    }
+	class GameViewHolder extends RecyclerView.ViewHolder {
 
-    private class MyOnClickListener implements View.OnClickListener {
+		TextView gameId, roundId, championshipId, homeTeamId, awayTeamId;
+		// ImageView imageView;
 
-        @Override
-        public void onClick(View view) {
-            int itemPosition = rvGameweek.getChildLayoutPosition(view);
-            int item = gamesList.get(itemPosition).getScore1();
-        }
-    }
+		public GameViewHolder(View itemView) {
+			super(itemView);
+
+			// gameId = itemView.findViewById(R.id.TODO);
+			// championshipId = itemView.findViewById(R.id.TODO);
+			roundId = itemView.findViewById(R.id.gameweekDashBetweenScores);
+			homeTeamId = itemView.findViewById(R.id.gameweek_preview_team1_score);
+			awayTeamId = itemView.findViewById(R.id.gameweek_preview_team2_score);
+			// imageView = itemView.findViewById(R.id.imageView);
+		}
+	}
 }
+
