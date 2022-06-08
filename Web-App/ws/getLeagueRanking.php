72e84ef9ad7fa2b98ad76ee9fea572958726aa42
iosif199 <iosifsaad@gmail.com>
2022-06-08 18:49:30 +0300

    Updated the "getLeagueRanking" web service to PDO, and made some security improvemnts

--- Web-App/ws/getLeagueRanking.php
+++ Web-App/ws/getLeagueRanking.php
@@ -1,22 +1,49 @@
 <?php
-	//GET parameters: lang (language), cid (championship_id)
-	
-	$data = array();
-	$lang = $_GET["lang"];
-	$championship_id = $_GET["cid"];
 
-	$host="localhost";
-	$uname="root";
-	$pass="";
-	$dbname="basketball_db";
-	
-	$dbh = mysqli_connect($host,$uname,$pass) or die("cannot connect");
-	mysqli_select_db($dbh, $dbname);
-			
-	$sql = "
+/**
+ * Web service which returns the league's ranking.
+ * The data is returned as a JSON file.
+ * 
+ * As parameters, it requires:
+ * 		lang	-> The language in which the results will be returned.
+ * 		cid		-> The championship's ID in the database
+ * 
+*/
+
+
+// Needed for the database connection
+require_once '../resources/config.php';
+
+// This Web Service returns it's data as a JSON, specify it
+header('Content-Type: application/json');
+
+// Check if all the necessary parameters are given
+if(	isset($_GET['lang']) && !empty($_GET['lang']) &&
+	isset($_GET['cid'])  && !empty($_GET['cid']))
+{
+	if($_GET['lang'] === 'en') {
+		$lang_stmt = 'name_en';
+	}
+	else if($_GET['lang'] === 'gr') {
+		$lang_stmt = 'name_gr';
+	}
+	else {
+		die('Invalid language');
+	}
+
+	$championship_id = $_GET['cid'];
+}
+
+// Connect to the database
+$dbh = connectDB();
+
+// The array which will be turned into the returned JSON
+$data = array();
+
+$sql = "
 SELECT logo, name, matches_played, (2*wins+(matches_played-wins)) as points, wins, (matches_played-wins) AS losses
 FROM
-    (SELECT logo_path AS logo, name_".$lang. " AS name, matches_played, CASE WHEN matches_won IS NULL THEN 0 ELSE matches_won END As wins
+    (SELECT logo_path AS logo, " . $lang_stmt . " AS name, matches_played, CASE WHEN matches_won IS NULL THEN 0 ELSE matches_won END As wins
     FROM team
     LEFT JOIN 
         (SELECT team_id, count(*) AS matches_played
@@ -37,7 +64,7 @@ FROM
                 (SELECT t.championship_id, t.round_id, t.game_id, t.team_id, SUM(t.quarter_score) AS total_score, g.away_team_id
                 FROM `team_score_per_quarter` t
                 JOIN game g ON t.round_id = g.round_id AND t.game_id = g.id AND t.team_id!=g.away_team_id
-				WHERE t.championship_id = ".$championship_id." 
+				WHERE t.championship_id = :cid 
                 GROUP BY round_id,game_id,team_id) AS h_team
             JOIN
                 (SELECT t.round_id, t.game_id, t.team_id, SUM(t.quarter_score) AS total_score
@@ -48,21 +75,41 @@ FROM
     GROUP BY winner) AS win_count ON team.id = win_count.winner) AS b
 ORDER BY points DESC";
 
-	$result = mysqli_query($dbh, $sql);
-	while ($row = mysqli_fetch_array($result)) { 
-		$nested_data = array();
-		$nested_data['logo'] = $row['logo'];
-		$nested_data['name'] = $row['name'];
-		
-		$nested_data['matches_played'] = $row['matches_played'];
-		$nested_data['points'] = $row['points'];
-		$nested_data['wins'] = $row['wins'];
-		$nested_data['losses'] = $row['losses'];
-		
-		$data[$row['name']] = $nested_data;
-	}
+// Prepare the statement
+$stmt = $dbh->prepare($sql);
 
-	header("Content-Type: application/json");
-	echo json_encode($data);
-	mysqli_close($dbh);
+// Bind the parameters
+$stmt->bindParam(':cid',	$championship_id, PDO::PARAM_INT);
+
+// Execute the statement and fetch the results
+try {
+	$stmt->execute();
+	$result = $stmt->fetchAll();
+}
+catch(PDOException $ex) {
+	echo 'ERROR while fetching the results. Reason: ' . $ex->getMessage();
+	die();
+}
+
+// Convert the returned results to a JSON compatible array
+foreach($result as $row) {
+	$nested_data = array();
+
+	$nested_data['logo'] = $row['logo'];
+	$nested_data['name'] = $row['name'];
+	
+	$nested_data['matches_played']	= $row['matches_played'];
+	$nested_data['points']			= $row['points'];
+	$nested_data['wins']			= $row['wins'];
+	$nested_data['losses']			= $row['losses'];
+	
+	$data[$row['name']] = $nested_data;
+}
+
+// Print the data as JSON
+echo json_encode($data);
+
+// Disconnect from the database
+unset($dbh);
+
 ?>
