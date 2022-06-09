bbee054bf533c2045dc1032fe6858e5947f12e48
iosif199 <iosifsaad@gmail.com>
2022-06-09 18:36:35 +0300

    Minor fix for the image display

--- Web-App/public_html/admin/createLeague.php
+++ Web-App/public_html/admin/createLeague.php
@@ -79,7 +79,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 				}
 
 				$conn = connectDB();
-				$data = $conn->query("SELECT * FROM team")->fetchAll();
+				$data = $conn->query('SELECT id, name_gr, logo_path FROM team')->fetchAll();
 					
 				if($data != null) {
 					echo '<form class="row" method="POST" action="'. htmlspecialchars($_SERVER['PHP_SELF']) . '">' . "\n";
@@ -88,9 +88,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 						echo '<div class="col-md-2 border pb-3 m-1 text-center">' . "\n";
 						echo '	<div class="custom-control custom-checkbox image-checkbox">' . "\n";
 						echo '		<input type="checkbox" name="t[]" value="' . $row['id'] . '" class="custom-control-input" id=' . $row['id'] . '>' . "\n";
-						echo '		<span class="lead mb-3"> '. $row['name_en'] . '</span><br>' . "\n";
+						echo '		<span class="lead mb-3"> '. $row['name_gr'] . '</span><br>' . "\n";
 						echo '		<label class="custom-control-label" for="'. $row['id'] . '">' . "\n";
-						echo '			<img src="'. $_SERVER['HTTP_HOST'] . $row['logo_path'] . '" alt="team- '. $row['id'] . '" class="img-fluid" />' . "\n";
+						echo '			<img src="' . $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $row['logo_path'] . '" alt="team-'. $row['id'] . '" class="img-fluid" />' . "\n";
 						echo '		</label>' . "\n";
 						echo '	</div>' . "\n";
 						echo '</div>' . "\n";
@@ -102,7 +102,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 					echo '	<button type="submit" class="btn btn-success mt-5 me-3">Δημιουργία Πρωταθλήματος</button>' . "\n";
 					echo '</div>' . "\n";
 				}
-				else { //There are no teams in the database
+				else { // There are no teams in the database
 					displayWarningBanner('Δεν βρέθηκαν ομάδες για να συμπεριληφθούν στο πρωτάθλημα.' .
 						'<br/><a class="alert-link" href='. AREF_ADMIN_CREATE_TEAM .'>Δημιουργήστε</a> ομάδες και ξαναπροσπαθήστε');
 				}
