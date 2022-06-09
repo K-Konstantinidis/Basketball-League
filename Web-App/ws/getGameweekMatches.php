d42023aab31d64473dc91c2e53462063d0541544
iosif199 <iosifsaad@gmail.com>
2022-06-09 17:09:53 +0300

    Updated the parameters and changed the SQL query

--- Web-App/ws/getGameweekMatches.php
+++ Web-App/ws/getGameweekMatches.php
@@ -5,6 +5,7 @@
  * The data is returned as a JSON file.
  * 
  * As parameters, it requires:
+ * 		cid		-> The championship's ID in the database
  * 		rid		-> The round's ID in the database
  * 
 */
@@ -13,15 +14,18 @@
 require_once '../resources/config.php';
 
 // This Web Service returns it's data as a JSON, specify it
-header("Content-Type: application/json");
+header('Content-Type: application/json');
 
 // Check if all the necessary parameters are given
-if(isset($_GET['rid'])) {
-	$round_id = $_GET['rid'];
+if( isset($_GET['cid']) && !empty($_GET['cid']) &&
+	isset($_GET['rid']) && !empty($_GET['rid'])
+) {
+	$championship_id = $_GET['cid'];
+	$round_id		 = $_GET['rid'];
 }
 else {
 	// An empty JSON will be returned if the parameter is not given
-	die();
+	die('Not all of the necessary parameters were passed');
 }
 
 // Connect to the database
@@ -32,23 +36,23 @@ $data = array();
 
 $sql =
 "SELECT h_team_info.rid AS round, h_team_info.gid AS game, 
-	h_team_info.logo AS home_logo, h_team_info.name as home_team, h_team_info.total_score AS home_team_score, 
-	a_team_info.logo AS away_logo, a_team_info.name as away_team, a_team_info.total_score AS away_team_score, h_team_info.status AS game_status
+	h_team_info.logo AS home_logo, h_team_info.total_score AS home_team_score, 
+	a_team_info.logo AS away_logo, a_team_info.total_score AS away_team_score, h_team_info.game_status AS game_status
 FROM 
-	(SELECT q_score.round_id AS rid, q_score.game_id AS gid, h_team.name_gr AS name, h_team.logo_path AS logo, SUM(quarter_score) AS total_score, g.status AS status
+	(SELECT q_score.round_id AS rid, q_score.game_id AS gid, h_team.logo_path AS logo, SUM(quarter_score) AS total_score, g.game_status AS game_status
 	FROM `team_score_per_quarter` AS q_score
 	JOIN team h_team ON q_score.team_id = h_team.id
 	JOIN game g ON (g.championship_id = q_score.championship_id AND g.round_id = q_score.round_id AND g.id = q_score.game_id AND g.home_team_id = q_score.team_id)
-	WHERE q_score.round_id = :round_id
-	GROUP BY q_score.round_id, q_score.game_id, q_score.team_id
+	WHERE q_score.championship_id = :cid AND q_score.round_id = :rid
+	GROUP BY q_score.championship_id, q_score.round_id, q_score.game_id, q_score.team_id
 	ORDER BY q_score.round_id, q_score.game_id, q_score.team_id, q_score.quarter) AS h_team_info
 JOIN 
-	(SELECT q_score.round_id AS rid, q_score.game_id AS gid, a_team.name_gr AS name, a_team.logo_path AS logo, SUM(quarter_score) AS total_score
+	(SELECT q_score.round_id AS rid, q_score.game_id AS gid, a_team.logo_path AS logo, SUM(quarter_score) AS total_score
 	FROM `team_score_per_quarter` AS q_score
 	JOIN team a_team ON q_score.team_id = a_team.id
 	JOIN game g ON (g.championship_id = q_score.championship_id AND g.round_id = q_score.round_id AND g.id = q_score.game_id AND g.away_team_id = q_score.team_id)
-	WHERE q_score.round_id = :round_id
-	GROUP BY q_score.round_id, q_score.game_id, q_score.team_id
+	WHERE q_score.championship_id = :cid AND q_score.round_id = :rid
+	GROUP BY q_score.championship_id, q_score.round_id, q_score.game_id, q_score.team_id
 	ORDER BY q_score.round_id, q_score.game_id, q_score.team_id, q_score.quarter) AS a_team_info
 ON (h_team_info.rid = a_team_info.rid AND h_team_info.gid = a_team_info.gid)";
 
@@ -56,7 +60,8 @@ ON (h_team_info.rid = a_team_info.rid AND h_team_info.gid = a_team_info.gid)";
 $stmt = $dbh->prepare($sql);
 
 // Bind the parameters
-$stmt->bindParam(':round_id', $round_id, PDO::PARAM_INT);
+$stmt->bindParam(':cid', $championship_id,	PDO::PARAM_INT);
+$stmt->bindParam(':rid', $round_id,			PDO::PARAM_INT);
 
 // Execute the statement and fetch the results
 try {
@@ -75,10 +80,8 @@ foreach($result as $row) {
 	$nested_data['round']			= $row['round'];
 	$nested_data['game']			= $row['game'];
 	$nested_data['home_logo']		= $row['home_logo'];
-	$nested_data['home_team']		= $row['home_team'];
 	$nested_data['home_team_score']	= $row['home_team_score'];
 	$nested_data['away_logo']		= $row['away_logo'];
-	$nested_data['away_team']		= $row['away_team'];
 	$nested_data['away_team_score']	= $row['away_team_score'];
 	$nested_data['game_status']		= $row['game_status'];
 	
