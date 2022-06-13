ce06bb41960e9e7ff298fc14e2884e9c4a3e2db9
Giannis <johhni97@gmail.com>
2022-06-13 21:24:13 +0300

    Fixed Top5 spinner and placements

--- Mob-App/app/src/main/java/com/example/esake/FragmentTop5.java
+++ Mob-App/app/src/main/java/com/example/esake/FragmentTop5.java
@@ -1,10 +1,12 @@
 package com.example.esake;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -52,9 +54,10 @@ public class FragmentTop5 extends Fragment implements AdapterView.OnItemSelected
 //    ImageView myImage;
 //    private String imageUri;
 	private Connector top5;
+	private Connector weeks;
 	private ImageView imageTop5;
 	private TextView nameTop5, posTop5, ratingTop5, pointsTop5;
-
+	private String round_id;
 
 
     @Override
@@ -62,28 +65,52 @@ public class FragmentTop5 extends Fragment implements AdapterView.OnItemSelected
         //Get the view
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_top5, null);
 
+		weeks = new Connector(myIP.getIp(),"gameweeks",null);
         //Get the spinner from the xml.
         Spinner dropdown = (Spinner) view.findViewById(R.id.spinner);
 
-		//Find the recyclerView
-		RecyclerView recyclerView = view.findViewById(R.id.recViewTop5);
-		//Create an adapter
-		Top5Adapter adapter;
-		//Create a list for the team ranking
-		List<Top5> top5List = new ArrayList<>();
+		ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, weeks.getWeeks());
+		dropdown.setAdapter(spinnerAdapter);
 
-		//Set a vertical layout manager
-		//If you are in an Activity class pass 'this'. But since
-		//we are in a Fragment Class we have to pass getContext()
-		recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
 
-		//Make a connection with the database via php
-		Connector lr = new Connector(myIP.getIp(), "top5"); //Tha ginei dynamika gia na allazei to round
+		dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+		{
+			@SuppressLint("SetTextI18n")
+			@Override
+			public void onItemSelected(AdapterView adapter, View v, int i, long lng) {
 
-		top5List = lr.getTop5();
+				String round = dropdown.getSelectedItem().toString();
+				round_id = round.substring(round.length() - 2);
 
-		adapter = new Top5Adapter(getContext(), top5List);
-		recyclerView.setAdapter(adapter);
+				//Find the recyclerView
+				RecyclerView recyclerView = view.findViewById(R.id.recViewTop5);
+				//Create an adapter
+
+				//Create a list for the team ranking
+				List<Top5> top5List = new ArrayList<>();
+
+				Top5Adapter adapter2;
+
+				//Set a vertical layout manager
+				//If you are in an Activity class pass 'this'. But since
+				//we are in a Fragment Class we have to pass getContext()
+				recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+				//Make a connection with the database via php
+				Connector lr = new Connector(myIP.getIp(), "top5", round_id); //Tha ginei dynamika gia na allazei to round
+
+				top5List = lr.getTop5();
+
+				adapter2 = new Top5Adapter(getContext(), top5List);
+				recyclerView.setAdapter(adapter2);
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> parentView)
+			{}
+		});
+
+
+
 
         return view;
     }
