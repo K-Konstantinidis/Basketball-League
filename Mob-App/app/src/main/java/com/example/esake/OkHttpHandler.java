a2d60ea001a29b497d3ffc822cf4fb4a1c05a315
Giannis <johhni97@gmail.com>
2022-06-17 23:04:37 +0300

    Fixed something on getDataForFPlayersTabbed in handler

--- Mob-App/app/src/main/java/com/example/esake/OkHttpHandler.java
+++ Mob-App/app/src/main/java/com/example/esake/OkHttpHandler.java
@@ -229,7 +229,7 @@ public class OkHttpHandler {
 				String logo = json.getJSONObject(id).getString("logo");
 				String surname= json.getJSONObject(id).getString("surname");
 				String rating = json.getJSONObject(id).getString("rating");
-				String totalPoints = json.getJSONObject(id).getString("total_shots");
+				String totalShots = json.getJSONObject(id).getString("total_shots");
 				String twoPointsIn = json.getJSONObject(id).getString("perc_2_in");
 				String threePointsIn = json.getJSONObject(id).getString("perc_3_in");
 				String freethrowsIn = json.getJSONObject(id).getString("perc_freethrows_in");
@@ -241,7 +241,7 @@ public class OkHttpHandler {
 				String fouls = json.getJSONObject(id).getString("total_fouls");
 
 				//Code to add from Json to Screen
-				PlayerStats pStats = new PlayerStats(logo, surname, Integer.parseInt(totalPoints),
+				PlayerStats pStats = new PlayerStats(logo, surname, Integer.parseInt(totalShots),
 					Integer.parseInt(rating), Double.parseDouble(twoPointsIn),
 					Double.parseDouble(threePointsIn), Double.parseDouble(freethrowsIn),
 					Integer.parseInt(totalRebounds), Integer.parseInt(assists),
