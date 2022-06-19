44527561a91d998f35f24da2b8284f400dffcb3b
Ntinos <konstantinides.kon@gmail.com>
2022-06-19 18:03:40 +0300

    Simplified Code - Added Strings

--- Mob-App/app/src/main/java/com/example/esake/FragmentGameManagementStatsManager.java
+++ Mob-App/app/src/main/java/com/example/esake/FragmentGameManagementStatsManager.java
@@ -11,241 +11,145 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-public class FragmentGameManagementStatsManager extends Fragment{
+import java.util.ArrayList;
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class FragmentGameManagementStatsManager extends Fragment {
+
+	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+	private static final String ARG_PARAM1 = "param1";
+	private static final String ARG_PARAM2 = "param2";
 
 //    private String mParam1;
 //    private String mParam2;
-    private boolean flag;
 
-    public FragmentGameManagementStatsManager() {
-        // Required empty public constructor
-    }
+	public FragmentGameManagementStatsManager() {
+		// Required empty public constructor
+	}
 
-    public static FragmentGameManagementStatsManager newInstance(String param1, String param2) {
-        FragmentGameManagementStatsManager fragment = new FragmentGameManagementStatsManager();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+	public static FragmentGameManagementStatsManager newInstance(String param1, String param2) {
+		FragmentGameManagementStatsManager fragment = new FragmentGameManagementStatsManager();
+		Bundle args = new Bundle();
+		args.putString(ARG_PARAM1, param1);
+		args.putString(ARG_PARAM2, param2);
+		fragment.setArguments(args);
+		return fragment;
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 //        if (getArguments() != null) {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
 //        }
-    }
+	}
 
-    @SuppressLint("InflateParams")
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //Get the view
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_game_management_stats_manager, null);
-        //Get the radio group with the 10 players (5 home + 5 away)
+	@SuppressLint("InflateParams")
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		//Get the view
+		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_game_management_stats_manager, null);
+		//Get the radio group with the 10 players (5 home + 5 away)
 		ToggleButtonGroupTableLayout rdg1 = root.findViewById(R.id.radGroup_player);
-        //Get the buttons
-        Button btn2pts = root.findViewById(R.id.button_2points);
-        Button btn2ptsM = root.findViewById(R.id.button_2pointsMissed);
-        Button btn3pts = root.findViewById(R.id.button_3points);
-        Button btn3ptsM = root.findViewById(R.id.button_3pointsMissed);
-        Button btnFT = root.findViewById(R.id.button_freeThrow);
-        Button btnFTM = root.findViewById(R.id.button_freeThrowMissed);
-        Button btnAssist = root.findViewById(R.id.button_assist);
-        Button btnFoul = root.findViewById(R.id.button_foul);
-		Button btnTurnOver = root.findViewById(R.id.button_turnover);
-        Button btnReboundAtt = root.findViewById(R.id.button_rebound);
-		Button btnReboundDef = root.findViewById(R.id.button_reboundDefensive);
-        Button btnBlock = root.findViewById(R.id.button_block);
-        Button btnSteal = root.findViewById(R.id.button_steal);
-        //Get the radio group with the 5 opponents
-        ToggleButtonGroupTableLayout radioOpp = root.findViewById(R.id.radGroup_choose_opponent);
-        flag = false;
+		//Get the radio group with the 5 opponents
+		ToggleButtonGroupTableLayout radioOpp = root.findViewById(R.id.radGroup_choose_opponent);
+		ArrayList<Button> buttonsList = new ArrayList<>();
 
-        btn2pts.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
+		buttonsList.add(root.findViewById(R.id.button_2points));
+		buttonsList.add(root.findViewById(R.id.button_2pointsMissed));
+		buttonsList.add(root.findViewById(R.id.button_3points));
+		buttonsList.add(root.findViewById(R.id.button_3pointsMissed));
+		buttonsList.add(root.findViewById(R.id.button_freeThrow));
+		buttonsList.add(root.findViewById(R.id.button_freeThrowMissed));
+		buttonsList.add(root.findViewById(R.id.button_assist));
+		buttonsList.add(root.findViewById(R.id.button_foul));
+		buttonsList.add(root.findViewById(R.id.button_turnover));
+		buttonsList.add(root.findViewById(R.id.button_rebound));
+		buttonsList.add(root.findViewById(R.id.button_reboundDefensive));
+		buttonsList.add(root.findViewById(R.id.button_block));
+		buttonsList.add(root.findViewById(R.id.button_steal));
 
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " ευστοχεί σε σουτ 2 πόντων", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-        });
+		for(Button b : buttonsList) {
+			b.setOnClickListener(view -> {
+				String text = (String) b.getText();
+				RadioButton player = root.findViewById(rdg1.getCheckedRadioButtonId());
+				setVisibility(radioOpp, text.equals("STL"));
 
-        btn2ptsM.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
+				if(player.isChecked()) {
+					switch (text) {
+						case "+2 \nPTS":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " ευστοχεί σε σουτ 2 πόντων", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "2 PTS Miss":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " αστοχεί σε σουτ 2 πόντων", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "+3 \nPTS":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " ευστοχεί σε σουτ 3 πόντων", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "3 PTS Miss":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " αστοχεί σε σουτ 3 πόντων", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "Free Throw":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " βάζει μία ελεύθερη βολή", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "FT Miss":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " χάνει μία ελεύθερη βολή", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "AST":
+							Toast.makeText(getContext(), "Η ασσίστ ανήκει στον " + player.getText(), Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "Foul":
+							Toast.makeText(getContext(), "Υπάρχει σφύριγμα για φάουλ που χρεώνεται στον " + player.getText(), Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "TO":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " υποπίπτει σε λάθος και χάνει την κατοχή της μπάλας", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "OFFENSIVE REB":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " μαζεύει το επιθετικό ριμπάουντ", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "DEFENSIVE REB":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " μαζεύει το αμυντικό ριμπάουντ", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "BLK":
+							Toast.makeText(getContext(), "Ο " + player.getText() + " κόβει τον αντίπαλό του", Toast.LENGTH_SHORT).show();
+							player.setChecked(false);
+							break;
+						case "STL":
+							RadioButton opponent = root.findViewById(radioOpp.getCheckedRadioButtonId());
+							if (opponent != null) {
+								Toast.makeText(getContext(), "Ο " + player.getText() + " κλέβει την μπάλα από τον " + opponent.getText(), Toast.LENGTH_SHORT).show();
+								player.setChecked(false);
+								opponent.setChecked(false);
+							} else
+								Toast.makeText(getContext(), "You must select an opponent before the action", Toast.LENGTH_SHORT).show();
+							break;
+					}
+				} else
+					selectPlayer();
+			});
+		}
+		return root;
+	}
 
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " αστοχεί σε σουτ 2 πόντων", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-        });
+	private void setVisibility(ToggleButtonGroupTableLayout radioOpp, boolean flag) {
+		if(flag)
+			radioOpp.setVisibility(View.VISIBLE);
+		else
+			radioOpp.setVisibility(View.GONE);
+	}
 
-        btn3pts.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " ευστοχεί σε σουτ 3 πόντων", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-		});
-
-        btn3ptsM.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " αστοχεί σε σουτ 3 πόντων", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-        });
-
-        btnFT.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " βάζει μία ελεύθερη βολή", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-        });
-
-        btnFTM.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " χάνει μία ελεύθερη βολή", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-        });
-
-        btnAssist.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Η ασσίστ ανήκει στον " + player.getText(), Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-        });
-
-        btnFoul.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Υπάρχει σφύριγμα για φάουλ που χρεώνεται στον " + player.getText(), Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-        });
-
-		btnTurnOver.setOnClickListener(view -> {
-			if(flag)
-				radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " υποπίπτει σε λάθος και χάνει την κατοχή της μπάλας", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-		});
-
-		btnReboundAtt.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " μαζεύει το επιθετικό ριμπάουντ", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-        });
-
-		btnReboundDef.setOnClickListener(view -> {
-			if(flag)
-				radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " μαζεύει το αμυντικό ριμπάουντ", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-		});
-
-        btnBlock.setOnClickListener(view -> {
-            if(flag)
-                radioOpp.setVisibility(View.GONE);
-
-			RadioButton player = getPlayer(rdg1, root);
-			if(player != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " κόβει τον αντίπαλό του", Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-			}
-			else
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-        });
-
-        btnSteal.setOnClickListener(view -> {
-            radioOpp.setVisibility(View.VISIBLE);
-            flag = true;
-
-			RadioButton player = getPlayer(rdg1, root);
-			RadioButton opponent = getPlayer(radioOpp, root);
-
-			if(player != null && opponent != null) {
-				Toast.makeText(getContext(), "Ο " + player.getText() + " κλέβει την μπάλα από τον " + opponent.getText(), Toast.LENGTH_SHORT).show();
-				player.setChecked(false);
-				opponent.setChecked(false);
-			}
-			else if(player == null)
-				Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
-			else
-				Toast.makeText(getContext(), "You must select an opponent before the action", Toast.LENGTH_SHORT).show();
-		});
-
-        return root;
-    }
-
-    public RadioButton getPlayer(ToggleButtonGroupTableLayout rdg, ViewGroup root){
-		return root.findViewById(rdg.getCheckedRadioButtonId());
+	private void selectPlayer() {
+		Toast.makeText(getContext(), "You must select a player before the action", Toast.LENGTH_SHORT).show();
 	}
 }
